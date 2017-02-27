414d6ad9f0f7115c062d24a7ab442723ac1deaef
paulhoets <paulhoets@gmail.com>
2017-02-27 17:13:23 +0200

    Add project files.

---
+++ MVVMPractice.UnitTests/MainViewModelTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MVVMPractice.UnitTests
+{
+    [TestClass]
+    public class MainViewModelTests
+    {
+        [TestMethod]
+        public void SayHelloCommandTest()
+        {
+            //arrange
+            var mainViewmodel = new MainViewModel();
+            mainViewmodel.Name = "Paul";
+            //act
+            mainViewmodel.SayHelloCommand.Execute(null);
+            //assert
+            Assert.AreEqual("Hello Paul",mainViewmodel.HelloMessage);
+        
+        }
+    }
+}
